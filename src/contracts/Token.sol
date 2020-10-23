407e264ec07becab44adabfa7f380fab2d280211
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-10-23 18:26:37 +0200

    delegated token transfers

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,8 +1,8 @@
 pragma solidity >=0.4.22 <0.8.0;
+
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Token {
-
     using SafeMath for uint;
 
     // Variables
@@ -10,10 +10,8 @@ contract Token {
     string public symbol = "DAPP";
     uint256 public decimals = 18;
     uint256 public totalSupply;
-    // Track balances
     mapping(address => uint256) public balanceOf;
     mapping(address => mapping(address => uint256)) public allowance;
-    // Send tokens
 
     // Events
     event Transfer(address indexed from, address indexed to, uint256 value);
@@ -24,24 +22,31 @@ contract Token {
         balanceOf[msg.sender] = totalSupply;
     }
 
-    function transfer(address _to, uint256 _value) public returns (bool success){
-        require(_to != address(0));
+    function transfer(address _to, uint256 _value) public returns (bool success) {
         require(balanceOf[msg.sender] >= _value);
-        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
-        balanceOf[_to] = balanceOf[_to].add(_value);
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
         return true;
     }
 
-    //  Approve tokens
-    function approve(address _spender, uint256 _value) public returns (bool success){
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != address(0));
+        balanceOf[_from] = balanceOf[_from].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
         require(_spender != address(0));
         allowance[msg.sender][_spender] = _value;
         emit Approval(msg.sender, _spender, _value);
         return true;
-
     }
 
-
-    // Transfer from 
-} 
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+        _transfer(_from, _to, _value);
+        return true;
+    }
+}
